b200af608499600a8d2b4b6e6d2c97f42a56e55d
cmj <>
2024-04-24 07:25:11 -0700

    date converter

---
+++ dates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# convert data file sequence numbers to dates
+# > set xdata time
+# > set timefmt "%Y-%m-%d"
+# > set xrange ["2024-02-23":"2024-04-23"]
+
+dateutils.dseq 2022-11-01 now -f '%F' | 
+  cat -n | \
+  while read num date; do 
+    grep "^${num} " master.dat | \
+    while read seq tweets likes; do 
+      echo "${date} ${tweets} ${likes}"
+    done
+  done
