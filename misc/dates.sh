0cbf525d4e53f58849d9cf83d370ee070c83d16e
cmj <>
2024-12-14 03:15:04 -0800

    update via cron

--- misc/dates.sh
+++ misc/dates.sh
@@ -7,7 +7,7 @@
 dateutils.dseq 2022-11-01 now -f '%F' | 
   cat -n | 
   while read num date; do 
-    grep "^${num} " master.dat | 
+    grep "^${num} " /home/cmj/gh/elontweets/master.dat | 
     while read seq tweets likes; do 
       echo "${date} ${tweets} ${likes}"
     done
